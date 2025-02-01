6af45aa41c1479b2a00fed8a73283365ab8d1839
rbmm <_dm_sh_@tutanota.com>
2025-02-02 01:53:14 +0200

    *

--- Load2/msgbox.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-int CustomMessageBox(HWND hWnd, PCWSTR lpText, PCWSTR lpszCaption, UINT uType);
-int ShowErrorBox(HWND hwnd, HRESULT dwError, PCWSTR lpCaption);
-HMODULE GetNtMod();
-ULONG GetLastErrorEx();
